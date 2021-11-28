72015aa808a62b0d151af6ab40c2377064b81c5d
Cenooo <94233626+Cenooo@users.noreply.github.com>
2021-11-28 12:19:22 +0100

    Update ft_strstr.c

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -9,3 +9,25 @@
 /*   Updated: 2021/11/28 12:18:50 by cpolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+char *ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (to_find[j] == str[i + j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (str + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
